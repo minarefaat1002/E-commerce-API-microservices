debea3e6b25156b5f34d9b692d0976842a329912
Mina Refaat <mina.samaan888@gmail.com>
2024-11-09 23:36:29 +0200

    setting django, celery, redis, postgresql

---
+++ ecommerce/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render, redirect
+from .tasks import count_task
+
+def home_view(request):
+    return render(request,'home.html')
+
+
+def count_to_10(request):
+    count_task.delay()
+    return redirect('home')
